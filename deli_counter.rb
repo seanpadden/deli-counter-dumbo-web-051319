a701355f434413a9a011565f00d574485b897193
Sean Padden <sean.padden7@gmail.com>
2019-05-07 14:46:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(name) 
+  line_array = []
+    if name.length == 0
+        puts "The line is currently empty."
+      else
+        name.each_with_index do |name, index|
+        line_array.push("#{index+1}. #{name}")
+        end
+        puts "The line is currently: #{line_array.join(" ")}"
+    end
+end
