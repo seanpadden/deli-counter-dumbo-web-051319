347caa93c80468ccf340a75284e520a600b99b7d
Sean Padden <sean.padden7@gmail.com>
2019-05-07 14:49:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(name)
   if name.length == 0
     puts "The line is currently empty."
   else
-    name.each_with_index do |name, index|
+    name.collect.each_with_index do |name, index|
       line_array.push("#{index+1}. #{name}")
     end
       puts "The line is currently: #{line_array.join(" ")}"
